4aea15c87300d84326b84ad44befeaa8aed6ee95
Alan Gutierrez <alan@prettyrobots.com>
2018-08-17 10:14:34 -0500

    Rename `database.js` to `router.js`.

--- t/router.t.js
+++ t/router.t.js
@@ -11,29 +11,29 @@ function prove (okay) {
     var client = []
     client.hostname = 'x'
     var Hash = require('../hash')
-    var Database = require('../database')
-    var database = new Database(client, 'a')
-    database.push({ hashed: hashes[0], body: 0 })
-    database.setBuckets([ 'a', 'b', 'a', 'a', 'b', 'b', 'a' ])
-    database.locate(hashes[0], '2/0')
-    database.locate(hashes[1], '2/0')
-    database.push({
+    var Router = require('../router')
+    var router = new Router(client, 'a')
+    router.push({ hashed: hashes[0], body: 0 })
+    router.setBuckets([ 'a', 'b', 'a', 'a', 'b', 'b', 'a' ])
+    router.locate(hashes[0], '2/0')
+    router.locate(hashes[1], '2/0')
+    router.push({
         hashed: hashes[0],
         gatherer: 'udp://127.0.0.1:8514/1/1',
         from: '1/0',
         body: 0
     })
-    database.push({ hashed: hashes[1], body: 1 })
-    database.push({ hashed: hashes[2], body: 2 })
-    database.push({
+    router.push({ hashed: hashes[1], body: 1 })
+    router.push({ hashed: hashes[2], body: 2 })
+    router.push({
         hashed: hashes[3],
         gatherer: 'udp://127.0.0.1:8514/1/2',
         from: '1/0',
         body: 3
     })
     okay(client.slice(), [], 'empty')
-    database.setBuckets([ 'a', 'b', 'c', 'a', 'b', 'b', 'a' ])
-    database.ready()
+    router.setBuckets([ 'a', 'b', 'c', 'a', 'b', 'b', 'a' ])
+    router.ready()
     okay(log.map(function (entry) {
         return entry.json.qualified
     }), [
