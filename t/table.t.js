52e58f582792d52cda2231a342d8027a27626651
Alan Gutierrez <alan@prettyrobots.com>
2018-09-07 14:55:53 -0500

    Update lookup table with arrival and departure.

---
+++ t/table.t.js
@@ -0,0 +1,38 @@
+require('proof')(4, prove)
+
+function prove (okay) {
+    var Table = require('../table')
+    var table = new Table
+    table.join(7, '1/0')
+    okay({
+        addresses: table.addresses,
+        buckets: table.buckets
+    }, {
+        addresses: [ '1/0' ],
+        buckets: [ '1/0', '1/0', '1/0', '1/0', '1/0', '1/0', '1/0' ]
+    }, 'join')
+    table.arrive('2/0')
+    okay({
+        addresses: table.addresses,
+        buckets: table.buckets
+    }, {
+        addresses: [ '1/0', '2/0' ],
+        buckets: [ '2/0', '2/0', '2/0', '2/0', '1/0', '1/0', '1/0' ]
+    }, 'first arrival')
+    table.arrive('3/0')
+    okay({
+        addresses: table.addresses,
+        buckets: table.buckets
+    }, {
+        addresses: [ '1/0', '2/0', '3/0' ],
+        buckets: [ '3/0', '2/0', '2/0', '2/0', '3/0', '3/0', '1/0' ]
+    }, 'second arrival')
+    table.depart('2/0')
+    okay({
+        addresses: table.addresses,
+        buckets: table.buckets
+    }, {
+        addresses: [ '1/0', '3/0' ],
+        buckets: [ '3/0', '1/0', '3/0', '1/0', '3/0', '3/0', '1/0' ]
+    }, 'second arrival')
+}
---
+++ table.js
@@ -0,0 +1,36 @@
+function Table () {
+    this.addresses = null
+    this.buckets = null
+}
+
+Table.prototype.join = function (count, address) {
+    this.addresses = [ address ]
+    this.buckets = new Array(count).fill(address)
+}
+
+Table.prototype.arrive = function (address) {
+    this.addresses.push(address)
+    var buckets = Math.ceil(this.buckets.length / this.addresses.length)
+    var remainder = buckets % (this.addresses.length - 1)
+    var dividend = Math.floor(buckets  / (this.addresses.length - 1))
+    var reassign = this.addresses.reduce(function (reassign, address) {
+        reassign[address] = dividend + (remainder-- == 0 ? 0 : 1)
+        return reassign
+    }, {})
+    for (var i = 0, I = this.buckets.length; i < I; i++) {
+        if (reassign[this.buckets[i]]-- > 0) {
+            this.buckets[i] = address
+        }
+    }
+}
+
+Table.prototype.depart = function (address) {
+    this.addresses.splice(this.addresses.indexOf(address), 1)
+    for (var i = 0, I = this.buckets.length, j = 0; i < I; i++) {
+        if (this.buckets[i] == address) {
+            this.buckets[i] = this.addresses[j++ % this.addresses.length]
+        }
+    }
+}
+
+module.exports = Table
