8e724e3f68ba53e2092ee85423f244a86dad43a0
Alan Gutierrez <alan@prettyrobots.com>
2019-02-18 10:23:18 -0600

    Simplify table redux bootstrap.

--- t/table.redux.t.js
+++ t/table.redux.t.js
@@ -2,13 +2,13 @@ require('proof')(1, prove)
 
 function prove (okay) {
     var Table = require('../table.redux')
-    var table = new Table()
+    var table = new Table(3)
 
     var shifter = table.events.shifter()
 
-    table.bootstrap('1/0')
+    table.bootstrap()
 
-    table.arrive('1/0')
+    table.arrive('1/0', '1/0')
 
     okay(shifter.shift(), {
         module: 'diffuser',
--- table.redux.js
+++ table.redux.js
@@ -6,35 +6,28 @@ var Interrupt = require('interrupt').createInterrupter('diffuser')
 
 function comparator (left, right) { return left.count - right.count }
 
-function Table (replicas) {
+function Table (redundancy) {
     this.events = new Queue
-    this._counts = {
-        primary: new RBTree(comparator),
-        secondary: new RBTree(comparator)
-    }
-    this._replicas = replicas
+    this._redundancy = redundancy
 }
 
-Table.prototype.bootstrap = function (self, count) {
-    this.self = self
-    this.properties = {}
+Table.prototype.bootstrap = function () {
     this.addresses = []
     this.buckets = []
 }
 
-Table.prototype.arrive = function (promise) {
-    if (this.buckets.length == 0) {
-        Interrupt.assert(promise == this.self, 'bad.bootstrap.promise')
+Table.prototype.arrive = function (self, promise) {
+    this.addresses.push(promise)
+    if (this.addresses.length == 1) {
+        Interrupt.assert(promise == self, 'bad.bootstrap.promise')
         this.buckets.push(promise)
-        this.addresses.push(promise)
-        this._counts.primary.insert({ count: 1, address: promise })
         this.events.push(JSON.parse(JSON.stringify({
             module: 'diffuser',
             method: 'balance',
             addresses: this.addresses,
             buckets: this.buckets
         })))
-    } else if (this.buckets.length < this._replicas + 1) {
+    } else if (this.buckets.length < this._redundancy) {
     }
 }
 
