02c102912d3d0356224c2756fef8aacbce0af20f
Alan Gutierrez <alan@prettyrobots.com>
2019-08-07 20:10:40 -0500

    Convert deploy script to ES6.

--- minikube/config/deploy.js
+++ minikube/config/deploy.js
@@ -1,9 +1,31 @@
-var fs = require('fs')
-var path = require('path')
-var yaml = require('js-yaml');
-var children = require('child_process')
+// The MIT License
+//
+// Copyright (c) 2018-2019 Alan Gutierrez
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
-var order = [
+const fs = require('fs')
+const path = require('path')
+const yaml = require('js-yaml');
+const children = require('child_process')
+
+const order = [
     'Namespace',
     'ConfigMap',
     'DaemonSet',
@@ -11,10 +33,10 @@ var order = [
     'Service'
 ]
 
-var files = fs.readdirSync(__dirname).filter(function (file) {
-    return /\.(?:json|yml)$/.test(file)
+const files = fs.readdirSync(__dirname).filter(function (file) {
+    return /\.(?:json|ya?ml)$/.test(file)
 }).map(function (file) {
-    var source = fs.readFileSync(path.join(__dirname, file), 'utf8')
+    const source = fs.readFileSync(path.join(__dirname, file), 'utf8')
     if (/\.yml$/.test(file)) {
         json = yaml.safeLoad(source)
     } else {
@@ -27,7 +49,7 @@ var files = fs.readdirSync(__dirname).filter(function (file) {
 }).sort(function (left, right) {
     return order.indexOf(left.json.kind) - order.indexOf(right.json.kind)
 }).forEach(function (file) {
-    var args = [ 'apply', '--record' ]
+    const args = [ 'apply', '--record' ]
     args.push('--filename', path.join(__dirname, file.name))
     children.spawnSync('kubectl', args, { stdio: [ 'inherit', 'inherit', 'inherit' ] })
 })
